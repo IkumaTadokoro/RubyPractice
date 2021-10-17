4bfeb79006f2116bd12a34d64bf1c50831bba3d8
IkumaTadokoro <tadokorodev@gmail.com>
2021-10-17 10:22:11 +0900

    不要な変数reverse_alphabet_indexを除去

--- UpperHeadLowerTail/upper_head_lower_tail.rb
+++ UpperHeadLowerTail/upper_head_lower_tail.rb
@@ -1,8 +1,7 @@
 
 class UpperHeadLowerTail
   def self.to_upper_head_lower_tail(word, alphabet)
-    reverse_alphabet_index = word.reverse.index(alphabet)
-    alphabet_index = reverse_alphabet_index ? word.size - reverse_alphabet_index - 1 : 0
-    reverse_alphabet_index ? word[0, alphabet_index].upcase + word[alphabet_index, word.size].downcase : word.upcase
+    alphabet_index = word.reverse.index(alphabet) ? word.size - word.reverse.index(alphabet) - 1 : nil
+    alphabet_index ? word[0, alphabet_index].upcase + word[alphabet_index, word.size].downcase : word.upcase
   end
 end
